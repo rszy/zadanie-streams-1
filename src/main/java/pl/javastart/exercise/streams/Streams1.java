736e6cb2f769b6b7173f3b676518523410c81444
Marcin Kunert <marcin.kunert@capgemini.com>
2017-03-10 18:48:07 +0100

    Added solutions

--- src/main/java/pl/javastart/exercise/streams/Streams1.java
+++ src/main/java/pl/javastart/exercise/streams/Streams1.java
@@ -14,11 +14,11 @@ public class Streams1 {
     }
 
     public List<Book> convertToBooks(List<String> titles) {
-        return null;
+        return titles.stream().map(title -> new Book(title)).collect(Collectors.toList());
     }
 
     // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
-        return null;
+        return titles.stream().filter(s -> s.startsWith("Gra")).map(title -> new Book(title)).collect(Collectors.toList());
     }
 }
