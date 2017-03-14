15ec277ccc54bafbacc589dfd6eecbf3e326a3f4
Ryszard Szymanski <ryszymanski@gmail.com>
2017-03-14 11:34:59 +0100

    Zadanie 1

--- src/main/java/pl/javastart/exercise/streams/Streams1.java
+++ src/main/java/pl/javastart/exercise/streams/Streams1.java
@@ -1,23 +1,30 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Streams1 {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+        return numberList.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+        return numberList.stream().filter(n -> n > 20).collect(Collectors.toList());
     }
 
     public List<Book> convertToBooks(List<String> titles) {
-        return null;
+        return titles.stream().map(new Function<String, Book>() {
+            @Override
+            public Book apply(String title) {
+                return new Book(title) ;
+            }
+        }).collect(Collectors.toList());
     }
 
     // Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
-        return null;
+        return convertToBooks(titles).stream().filter(title -> title.getTitle().startsWith("Gra")).collect(Collectors.toList());
     }
 }
--- src/main/java/pl/javastart/exercise/streams/Streams2.java
+++ src/main/java/pl/javastart/exercise/streams/Streams2.java
@@ -1,15 +1,23 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Streams2 {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
-        return null;
+        return ppl.stream().filter(person -> person.getAge()<18).map(person -> person.getName()).collect(Collectors.toList());
     }
 
     public List<User> convertPeopleToUsers(List<Person> people) {
-        return null;
+        return people.stream().map(new Function<Person, User>() {
+            @Override
+            public User apply(Person person) {
+                return new User(person.getName(), person.getAge(), person.getName()+"_"+person.getAge());
+            }
+        }).collect(Collectors.toList());
     }
 }
+
